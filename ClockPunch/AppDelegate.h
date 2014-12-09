b16ea1d96f764e61477fe5b9b17782635dd0d76c
ac <aldobest@gmail.com>
2014-12-10 00:32:42 +0100

    Initial Commit

---
+++ ClockPunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClockPunch
+//
+//  Created by Aldo Castro on 10/12/14.
+//  Copyright (c) 2014 Aldo Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClockPunch/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ClockPunch
+//
+//  Created by Aldo Castro on 10/12/14.
+//  Copyright (c) 2014 Aldo Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
