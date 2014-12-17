b67abb3feb965d14504b9c0ed50ad237b7949349
ac <ac@local.host>
2014-12-17 23:23:45 +0100

    adding ClockPunchVC class

---
+++ ClockPunch/ClockPunchVC.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ClockPunch
+//
+//  Created by Aldo Castro on 10/12/14.
+//  Copyright (c) 2014 Aldo Castro. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ClockPunchVC : UITableViewController
+
+@end
---
+++ ClockPunch/ClockPunchVC.m
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.m
+//  ClockPunch
+//
+//  Created by Aldo Castro on 10/12/14.
+//  Copyright (c) 2014 Aldo Castro. All rights reserved.
+//
+
+#import "ClockPunchVC.h"
+
+@implementation ClockPunchVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
